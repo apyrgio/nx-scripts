1d2e898f76ffae14cdfe7f918e3da17a878ebc41
Alex Pyrgiotis <apyrgio@cslab.ece.ntua.gr>
2012-11-12 23:03:11 +0200

    Moderate fix
    Unpack tarballs before installation

--- install.sh
+++ install.sh
@@ -9,6 +9,10 @@ sudo wget --content-disposition https://pithos.okeanos.grnet.gr/public/vz6zt 	#D
 sudo wget --content-disposition https://pithos.okeanos.grnet.gr/public/g6k63	#Download nxnode
 sudo wget --content-disposition https://pithos.okeanos.grnet.gr/public/2m8ns	#Download nxserver
 
+sudo tar zxvf nxclient-3.5.0-7.x86_64.tar.gz 
+sudo tar zxvf nxnode-3.5.0-9.x86_64.tar.gz 
+sudo tar zxvf nxserver-3.5.0-11.x86_64.tar.gz
+
 sudo /usr/NX/scripts/setup/nxnode --install 
 sudo /usr/NX/scripts/setup/nxserver --install
 

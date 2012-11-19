7db59779f6f546bf1a97956540b01798f955740f
Alex Pyrgiotis <apyrgio@cslab.ece.ntua.gr>
2012-11-19 10:54:41 +0200

    Support for Fedora distro

--- install.sh
+++ install.sh
@@ -4,17 +4,26 @@ txtrst=$(tput sgr0)     # Reset text color
 txtred=$(tput setaf 1)  # Make text red
 txtgrn=$(tput setaf 2)  # Make text green
 
+UBUNTU_DISTRO=`cat /etc/*-release | grep 'Ubuntu'`
+FEDORA_DISTRO=`cat /etc/*-release | grep 'Fedora'`
+
+if [[ -n $UBUNTU_DISTRO ]]; then
+	sudo -s
+elif [[ -n $FEDORA_DISTRO ]]; then
+	su
+else
+
 cd /usr
-sudo wget --content-disposition https://pithos.okeanos.grnet.gr/public/vz6zt 	#Download nxclient
-sudo wget --content-disposition https://pithos.okeanos.grnet.gr/public/g6k63	#Download nxnode
-sudo wget --content-disposition https://pithos.okeanos.grnet.gr/public/2m8ns	#Download nxserver
+wget --content-disposition https://pithos.okeanos.grnet.gr/public/vz6zt 	#Download nxclient
+wget --content-disposition https://pithos.okeanos.grnet.gr/public/g6k63	#Download nxnode
+wget --content-disposition https://pithos.okeanos.grnet.gr/public/2m8ns	#Download nxserver
 
-sudo tar zxvf nxclient-3.5.0-7.x86_64.tar.gz 
-sudo tar zxvf nxnode-3.5.0-9.x86_64.tar.gz 
-sudo tar zxvf nxserver-3.5.0-11.x86_64.tar.gz
+tar zxvf nxclient-3.5.0-7.x86_64.tar.gz 
+tar zxvf nxnode-3.5.0-9.x86_64.tar.gz 
+tar zxvf nxserver-3.5.0-11.x86_64.tar.gz
 
-sudo /usr/NX/scripts/setup/nxnode --install 
-sudo /usr/NX/scripts/setup/nxserver --install
+/usr/NX/scripts/setup/nxnode --install 
+/usr/NX/scripts/setup/nxserver --install
 
 if [[ -e /etc/init.d/nxserver ]]; then
 	echo "${txtgrn}NX Server seems to have been installed successfully${txtrst}"

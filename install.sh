7768810f7733a6e56b0edd79da24d85fdc439db0
Alex Pyrgiotis <apyrgio@cslab.ece.ntua.gr>
2012-11-19 13:27:23 +0200

    Fix privilege issue

--- install.sh
+++ install.sh
@@ -8,28 +8,18 @@ UBUNTU_DISTRO=`cat /etc/*-release | grep 'Ubuntu'`
 FEDORA_DISTRO=`cat /etc/*-release | grep 'Fedora'`
 
 if [[ -n $UBUNTU_DISTRO ]]; then
-	sudo -s
+	sudo -s "./su_install.sh"
 elif [[ -n $FEDORA_DISTRO ]]; then
-	su
+	su "./su_install.sh"
 else
 	echo "${txtred}Distro not supported.${txtrst}"
 	exit 1
 fi
-
-cd /usr
-wget --content-disposition https://pithos.okeanos.grnet.gr/public/vz6zt #Download nxclient
-wget --content-disposition https://pithos.okeanos.grnet.gr/public/g6k63	#Download nxnode
-wget --content-disposition https://pithos.okeanos.grnet.gr/public/2m8ns	#Download nxserver
-
-tar zxvf nxclient-3.5.0-7.x86_64.tar.gz 
-tar zxvf nxnode-3.5.0-9.x86_64.tar.gz 
-tar zxvf nxserver-3.5.0-11.x86_64.tar.gz
-
-/usr/NX/scripts/setup/nxnode --install 
-/usr/NX/scripts/setup/nxserver --install
 
 if [[ -e /etc/init.d/nxserver ]]; then
 	echo "${txtgrn}NX Server seems to have been installed successfully${txtrst}"
 else
 	echo "${txtred}Installation failed${txtrst}"
 fi
+
+
---
+++ su_install.sh
@@ -0,0 +1,14 @@
+#~ /bin/bash
+
+cd /usr
+wget --content-disposition https://pithos.okeanos.grnet.gr/public/vz6zt #Download nxclient
+wget --content-disposition https://pithos.okeanos.grnet.gr/public/g6k63	#Download nxnode
+wget --content-disposition https://pithos.okeanos.grnet.gr/public/2m8ns	#Download nxserver
+
+tar zxvf nxclient-3.5.0-7.x86_64.tar.gz 
+tar zxvf nxnode-3.5.0-9.x86_64.tar.gz 
+tar zxvf nxserver-3.5.0-11.x86_64.tar.gz
+
+/usr/NX/scripts/setup/nxnode --install 
+/usr/NX/scripts/setup/nxserver --install
+
